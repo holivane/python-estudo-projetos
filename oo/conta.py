324661df348754f965261689b9beefd9c3bdea17
Holivane <holivane.cabrini@maistodos.com.br>
2023-05-09 10:12:24 -0300

    Métodos para a classe conta

--- oo/conta.py
+++ oo/conta.py
@@ -1,7 +1,17 @@
 class Conta:
+
     def __init__(self, numero, titular, saldo, limite) -> None:
         print("Construindo objeto...")
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Olá {} seu saldo é {}".format(self.titular, self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
