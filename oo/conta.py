db83a17cde07dededdabb00217f3ac14b670710a
Holivane <holivane.cabrini@maistodos.com.br>
2023-05-16 11:01:18 -0300

    encapsulamento e propriedades

--- oo/conta.py
+++ oo/conta.py
@@ -1,17 +1,38 @@
 class Conta:
 
-    def __init__(self, numero, titular, saldo, limite) -> None:
-        print("Construindo objeto...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Olá {} seu saldo é {}".format(self.titular, self.saldo))
+        print("Olá {} seu saldo é {}".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+        print('Você transferiu {} para {}'.format(valor, destino))
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
