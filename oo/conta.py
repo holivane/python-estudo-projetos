55a9df4be4e5443562f8f91ac3c40d881c974f83
Holivane <holivane.cabrini@maistodos.com.br>
2023-05-17 18:45:56 -0300

    métodos

--- oo/conta.py
+++ oo/conta.py
@@ -13,13 +13,23 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponível_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponível_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} excede o limite".format(valor))
 
     def transfere(self, valor, destino):
-        self.saca(valor)
-        destino.deposita(valor)
-        print('Você transferiu {} para {}'.format(valor, destino))
+        if(self.__pode_sacar(valor)):
+            self.saca(valor)
+            destino.deposita(valor)
+            print('Você transferiu {} para {}'.format(valor, destino))
+        else:
+            print("O valor {} excede o limite".format(valor))
 
     @property
     def saldo(self):
@@ -36,3 +46,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {"BB": "001", "Caixa": "104","Bradesco": "237"}
