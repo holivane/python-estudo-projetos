e7baae4594127c97b239633db58bc14733b0cdc3
Holivane <holivane.cabrini@maistodos.com.br>
2023-05-23 11:00:31 -0300

    Herança

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,8 +1,7 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
+class Programa:
+    def __init__(self, nome, ano):
         self.__nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
         self.__likes = 0
 
     @property
@@ -20,32 +19,25 @@ class Filme:
     def dar_likes(self):
         self.__likes += 1
 
-class Serie:
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = temporadas
-        self.likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
 
 
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 
-vingadores = Filme('vingadores: guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}')
+atlanta.dar_likes()
+atlanta.dar_likes()
 
-atlanta = Filme('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
