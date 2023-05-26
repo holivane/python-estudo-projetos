666f61e0b9af70b4eadcd1e247dba9840bcbe448
Holivane <holivane.cabrini@maistodos.com.br>
2023-05-26 10:07:13 -0300

    Herança

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -38,17 +38,41 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    @property
+    def listagem(self):
+        return self.programas
+
+    @property
+    def tamanho(self):
+        return len(self.programas)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-listinha = [atlanta, vingadores]
+listinha = [atlanta, demolidor, tmep, vingadores]
+minha_playlist = Playlist('fim de semana', listinha)
 
-for programa in listinha:
+for programa in minha_playlist.listagem:
     print(programa)
+
+print(f'Tamanho: {minha_playlist.tamanho}')
