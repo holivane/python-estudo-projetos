7a8658b47a2208e8d0b4e6ea757e75a26ab8ea1f
Holivane <holivane.cabrini@maistodos.com.br>
2023-06-09 10:27:47 -0300

    Índice, fatiamento sem um dos argumentos, find, len

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,8 +1,21 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+list_parametros = ['quantidade', 'moedaOrigem', 'moedaDestino']
 print(url)
 
-url_base = url[0:19]
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_base)
-
-url_parametros = url[20:36]
 print(url_parametros)
+
+for parametro in list_parametros:
+  parametro_de_busca = parametro
+  indice_parametro = url_parametros.find(parametro_de_busca)
+  indice_valor = indice_parametro + len(parametro_de_busca) + 1
+  indice_e_comercial = url_parametros.find('&', indice_valor)
+  if indice_e_comercial == -1:
+      valor = url_parametros[indice_valor:]
+  else:
+      valor = url_parametros[indice_valor:indice_e_comercial]
+
+  print(valor)
