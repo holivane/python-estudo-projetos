d6ba21288fcf8bd7118a2591cf54d834d633ef20
Holivane <holivane.cabrini@maistodos.com.br>
2023-06-14 10:20:34 -0300

    Validações com RegEx, match, group

--- extrator-url/extrator_url.py
+++ extrator-url/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url) -> None:
         self.url = self.sanitiza_url(url)
@@ -12,6 +14,11 @@ class ExtratorURL:
     def valida_url(self):
       if not self.url:
         raise ValueError("A url está vazia")
+
+      padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+      match = padrao_url.match(url)
+      if not match:
+        raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
       indice_interrogacao = self.url.find('?')
@@ -33,7 +40,7 @@ class ExtratorURL:
         valor = self.get_url_parametro()[indice_valor:indice_e_comercial]
       return valor
 
-# extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
-extrator_url = ExtratorURL(None)
+url = "www.bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
 valor = extrator_url.get_valor_parametro('quantidade')
 print(valor)
---
+++ extrator-url/validador_url.py
@@ -0,0 +1,11 @@
+
+import re
+
+url = 'www.bytebank.com.br/cambio'
+padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("A URL não é válida.")
+
+print("A URL é válida")
