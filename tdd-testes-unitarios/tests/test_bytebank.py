44fc559598aecfab50476aeed5937428936a86c7
Holivane <holivane.cabrini@maistodos.com.br>
2023-07-27 08:43:15 -0300

    Marcadores para teste

--- tdd-testes-unitarios/tests/test_bytebank.py
+++ tdd-testes-unitarios/tests/test_bytebank.py
@@ -1,5 +1,6 @@
 from codigo.bytebank import Funcionario
 import pytest
+from pytest import mark
 
 
 class TestClass:
@@ -32,6 +33,7 @@ class TestClass:
 
         assert resultado == esperado
 
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
         entrada = 1000
         esperado = 100
@@ -41,6 +43,7 @@ class TestClass:
 
         assert resultado == esperado
 
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_100000_deve_retornar_exception(self):
         with pytest.raises(Exception):
             entrada = 100000
