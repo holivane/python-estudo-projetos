8864bebcf063214aa7a0ab6beb90ac7c8c69d5ea
Holivane <holivane.cabrini@maistodos.com.br>
2023-07-27 08:25:04 -0300

    Exception

--- tdd-testes-unitarios/codigo/bytebank.py
+++ tdd-testes-unitarios/codigo/bytebank.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Funcionario:
     def __init__(self, nome, data_nascimento, salario):
         self._nome = nome
@@ -26,8 +27,9 @@ class Funcionario:
         return nome_quebrado[-1]
 
     def _eh_socio(self):
-        sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
-        return (self._salario >=100000 and (self.sobrenome() in sobrenomes))
+        sobrenomes = ['Bragança', 'Windsor', 'Bourbon',
+                      'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
+        return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
 
     def decrescimo_salario(self):
         if self._eh_socio:
@@ -37,7 +39,7 @@ class Funcionario:
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('O salário é muito alto para receber um bônus')
         return valor
 
     def __str__(self):
